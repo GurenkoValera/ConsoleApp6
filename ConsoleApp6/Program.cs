3b5af3f976e666f9166e55f0ed3a3b82811c3bf6
valer <valer@DESKTOP-QD94NVR>
2024-10-06 17:37:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод четырехзначного числа
+        Console.Write("Введите четырехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Проверка, является ли число четырехзначным
+        if (number < 1000 || number > 9999)
+        {
+            Console.WriteLine("Число должно быть четырехзначным.");
+            return;
+        }
+
+        // Вычисление произведения цифр
+        int product = 1;
+        while (number > 0)
+        {
+            product *= number % 10;
+            number /= 10;
+        }
+
+        // Вывод результата
+        Console.WriteLine("Произведение цифр: " + product);
+    }
+}
